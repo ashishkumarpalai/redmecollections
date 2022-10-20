60f2bf04f0a5c1702ce4aaf6080a400c54da6d1a
Ashish Kumar Palai <ashishkumarpalai2000@gmail.com>
2022-10-20 22:11:59 +0530

    check the code

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+let str="ashish";
+for(let i=0;i<=str.length-1;i++){
+bag+=str[i]
+}
+if(str==bag){
+console.log("yes");
+}else{
+console.log("no");
---
+++ prime.js
@@ -0,0 +1,5 @@
+let num=12;
+let count=1
+for(let i=0;i<=num;i++){
+if(i%2==0)
+
